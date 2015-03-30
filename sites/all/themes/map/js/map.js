ae3e687eadf0d727d3d3e4e93788463d922eed52
root <root@DylanVM>
2015-03-30 10:59:24 -0600

    sliding menu now functional

--- sites/all/themes/map/js/map.js
+++ sites/all/themes/map/js/map.js
@@ -5,7 +5,12 @@
         map = new Map("map", {
           basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
           center: [-122.45, 37.75], // longitude, latitude
-          zoom: 13
+          zoom: 13,
+          sliderPosition: "top-right"
         });
       });
 
+
+    $(document).ready(function() {
+        $('.menu-link').bigSlide();
+    });
--- sites/all/themes/map/templates/page--map.tpl.php
+++ sites/all/themes/map/templates/page--map.tpl.php
@@ -8,9 +8,22 @@
  */
 ?>
 
+
+
   
   <div id="map"></div>
 
 
-
+<nav id="menu" class="panel" role="navigation">
+    <div class="menu-link-wrap">
+        <a href="#menu" class="menu-link">&#9776;</a>
+    </div>    
+    <ul>
+        <li><a href="#" class="option">Home</a></li>
+        <li><a href="#" class="option">The Ballad of El Goodo</a></li>
+        <li><a href="#" class="option">Thirteen</a></li>
+        <li><a href="#" class="option">September Gurls</a></li>
+        <li><a href="#" class="option">What's Going Ahn</a></li>
+    </ul>
+</nav>
 
